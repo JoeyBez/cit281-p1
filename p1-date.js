6d03de6d0db8121c6b50a565a2870bcea100b509
Joey Bezner <jmbez@comcast.net>
2024-06-07 16:19:57 -0700

    Create p1-date.js

---
+++ p1-date.js
@@ -0,0 +1,6 @@
+/*
+    CIT 281 Project 1
+    Name: Joey Bezner
+*/
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log(days[new Date().getDay()]);
